5658aaa217545d40cef38d997a91bfd27fe54529
Wez Furlong <wez@netevil.org>
2010-06-01 15:01:57 -0400

    really exclude excluded branches.
    fix case sensitivity on merge|revert commit log.
    Print list of excluded/included branches at the end of analysis.

--- util.php
+++ util.php
@@ -13,6 +13,9 @@ class Branch {
   public $is_tag = false;
   public $is_pure = true;
   public $bname;
+  /* record individual node paths modified against this branch so that
+   * we can later on figure out how to get at the content */
+  public $activity_by_path = array();
 
   function __construct($rev, $path) {
     $this->createdrev = $rev;
@@ -38,21 +41,19 @@ class Branch {
 
   function addActivity(SvnDumpReader $R) {
     $this->activity[$R->revision] = $R->revision;
-    if (!$this->is_pure) {
-      return;
-    }
     foreach ($R->nodes as $node) {
       if (is_child_of($node->path, $this->name)) {
+        $this->activity_by_path[$node->path][$R->revision] = $R->revision;
         switch ($node->action) {
           case 'add':
           case 'change':
+          case 'replace':
             $this->is_pure = false;
             break;
           case 'delete':
-            if ($node->path == $this->name) {
-              break;
+            if ($node->path != $this->name) {
+              $this->is_pure = false;
             }
-            $this->is_pure = false;
             break;
           default:
             echo "Unhandled action $node->action\n";
